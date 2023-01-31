41d78a6d5e77f2377709e73e8055f7317dfb76c6
Nataliia Chala <hatalochka.rv@gmail.com>
2023-01-31 20:04:11 +0000

    did styling of crew pages

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const navLinks=document.querySelectorAll(".crew-nav__link");
+// const navLinks=document.querySelectorAll(".crew-nav__link");
 
-navLinks.forEach((navLink)=>{
-    navLink.addEventListener('click', ()=>{
-        navLink.classList.toggle('active');
-    })
-})
+// navLinks.forEach((navLink)=>{
+//     navLink.addEventListener('click', ()=>{
+//         for(var i=0; i<navLinks.length; i++){
+//             navLinks[i].classList.remove('active');
+//         }
+//         navLink.classList.add('active');
+//     })
+// })
